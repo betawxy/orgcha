117d5e475041c92f472052e6e72bc9d13a4bc534
betawxy <betawxy@gmail.com>
2021-04-26 20:57:28 -0700

    header footer

--- web/components/pageWrapper.tsx
+++ web/components/pageWrapper.tsx
@@ -2,17 +2,19 @@ import Head from "next/head";
 
 export default function PageWrapper({ children }) {
   return (
-    <div className="flex flex-col justify-between w-screen h-screen">
-      <nav className="flex flex-none w-full h-20 bg-blue-800">
-        <div className="container mx-auto">
+    <div className="beta-page-wrapper">
+      <nav className="beta-hf-wrapper h-20 bg-blue-800">
+        <div className="beta-container">
           <div className="text-2xl text-white">Public OrgChart</div>
         </div>
       </nav>
       <main className="w-full mb-auto">
-        <div className="container mx-auto">{children}</div>
+        <div className="beta-container">{children}</div>
       </main>
-      <footer className="flex flex-none w-full h-32 bg-blue-100">
-        <div className="container mx-auto ">footer</div>
+      <footer className="beta-hf-wrapper h-32 bg-blue-100">
+        <div className="beta-container">
+          &copy; {new Date().getFullYear()} PublicOrgChart.com
+        </div>
       </footer>
     </div>
   );
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,9 +5,9 @@ import PageWrapper from "components/pageWrapper";
 export default function Home() {
   return (
     <PageWrapper>
-      <div className="h-80">home</div>
-      <div className="h-80">home</div>
-      <div className="h-80">home</div>
+      <div className="h-80 bg-green-100 w-full">home</div>
+      <div className="h-80 bg-green-100 w-full">home</div>
+      <div className="h-80 bg-green-100 w-full">home</div>
     </PageWrapper>
   );
 }
