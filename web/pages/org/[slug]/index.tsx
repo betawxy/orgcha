9ddf25a1a0ff0a6385590b662cd751cdcf692f68
betawxy <betawxy@gmail.com>
2021-04-26 23:04:13 -0700

    dynamic team

--- web/pages/org/[slug]/index.tsx
+++ web/pages/org/[slug]/index.tsx
@@ -3,20 +3,28 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 import PageWrapper from "components/pageWrapper";
+import { getOrg, getOrgTeam, TOrg, TPeople } from "store/data";
 
 export default function OrgPage() {
   const router = useRouter();
   const { slug } = router.query;
 
+  const org: TOrg = getOrg(slug as string);
+  const team: TPeople[] = getOrgTeam(slug as string);
+
   return (
     <PageWrapper>
-      <div className="text-xl">org page - {slug}</div>
-      <div>Team</div>
-      <span className="beta-link">
-        <Link href="/people/mark-zuckerberg-S8r3">Mark Zuckerberg</Link>
-      </span>
-      <div>Jobs</div>
+      <div className="text-xl my-6">Org - {org.name}</div>
+      <div className="border-b border-gray-300 mt-3">Team</div>
+      {team.map((people, key) => (
+        <div key={key} className="">
+          <span className="beta-link">
+            <Link href={`/people/${people.slug}`}>{people.name}</Link>
+          </span>
+        </div>
+      ))}
 
+      <div className="border-b border-gray-300 mt-3">Jobs</div>
       <span className="beta-link">
         <Link href={`/org/${slug}/jobs`}>Jobs</Link>
       </span>
--- web/store/data.ts
+++ web/store/data.ts
@@ -1,14 +1,25 @@
-export type TPeople = {};
+export type TPeople = {
+  slug: string;
+  name: string;
+};
 
 export type TOrg = {
   slug: string;
   name: string;
   about: string;
-  team: TPeople[];
+  team: string[];
 };
 
 export function getPopularOrgs(): Array<TOrg> {
   return [ORGS["facebook"], ORGS["us-federal-gov"]];
+}
+
+export function getOrg(slug: string): TOrg {
+  return ORGS[slug];
+}
+
+export function getOrgTeam(slug: string): Array<TPeople> {
+  return ORGS[slug]["team"].map((ps: string) => PEOPLE[ps]);
 }
 
 const ORGS: { [key: string]: TOrg } = {
@@ -17,13 +28,28 @@ const ORGS: { [key: string]: TOrg } = {
     name: "Facebook, Inc.",
     about:
       "Founded in 2004, Facebook’s mission is to give people the power to build community and bring the world closer together. Over 2 billion people use Facebook, Instagram, WhatsApp, or Messenger every month to stay connected with friends and family, to discover what’s going on in the world, and to share and express what matters to them.",
-    team: [],
+    team: ["mark-zuckerberg-S8r3", "chris-cox-89as"],
   },
   "us-federal-gov": {
     slug: "us-federal-gov",
     name: "US Federal Government",
     about: "...",
-    team: [],
+    team: ["joe-biden-9123"],
+  },
+};
+
+const PEOPLE: { [key: string]: TPeople } = {
+  "mark-zuckerberg-S8r3": {
+    slug: "mark-zuckerberg-S8r3",
+    name: "Mark Zuckerberg",
+  },
+  "chris-cox-89as": {
+    slug: "chris-cox-89as",
+    name: "Chris Cox",
+  },
+  "joe-biden-9123": {
+    slug: "joe-biden-9123",
+    name: "Joe Biden",
   },
 };
 
