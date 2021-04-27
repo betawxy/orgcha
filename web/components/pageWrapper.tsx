a3aed732523466ee6aaa3dfe26466d915c288077
betawxy <betawxy@gmail.com>
2021-04-26 20:32:17 -0700

    sticky footer

--- web/components/pageWrapper.tsx
+++ web/components/pageWrapper.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
 
-export default function C({ children }) {
+export default function PageWrapper({ children }) {
   return (
-    <div className="w-screen h-screen bg-gray-100">
-      <nav className="w-full h-40 bg-blue-800">
+    <div className="flex flex-col justify-between w-screen h-screen">
+      <nav className="flex flex-none w-full h-20 bg-blue-800">
         <div className="container mx-auto">
-          <div className="">Public OrgChart</div>
+          <div className="text-2xl text-white">Public OrgChart</div>
         </div>
       </nav>
-      <main className="w-full">
+      <main className="w-full mb-auto">
         <div className="container mx-auto">{children}</div>
       </main>
-      <footer className="w-full h-60">
+      <footer className="flex flex-none w-full h-32 bg-blue-100">
         <div className="container mx-auto ">footer</div>
       </footer>
     </div>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,5 +3,11 @@ import React from "react";
 import PageWrapper from "components/pageWrapper";
 
 export default function Home() {
-  return <PageWrapper>home</PageWrapper>;
+  return (
+    <PageWrapper>
+      <div className="h-80">home</div>
+      <div className="h-80">home</div>
+      <div className="h-80">home</div>
+    </PageWrapper>
+  );
 }
